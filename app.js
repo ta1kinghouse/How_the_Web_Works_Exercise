6a1a1ebc990be6d6f6f7318f478f213dc74b2fe7
Sam Gurango <samrenango@aol.com>
2023-01-02 19:56:11 -0800

    Added How the Web Works exercise

---
+++ app.js
@@ -0,0 +1,10 @@
+// Part One: Solidify Terminology
+//HTTP stands for hypertext transfer protool and is a protocol for how browsers and servers communicate.
+//URL stands for universal resource locator. It's the address for something on the web.
+//DNS stands for domain name system and it takes the host name and turns it into an IP address.
+//A query string is information such as info from forms that is provided to the server.
+//Two HTTP verbs GET and POST. GET makes requests without changing data in the server whereas POST does.
+//An HTTP request is the web browser resquesting a resource from a server.
+//An HTTP response is the server sending back a response in correspondance to the resource.
+//An HTTP header pass information between the server and the user.
+//When a URL is typed into a browser, the site is turned into and connected to an IP address on port 80. Then, using HTTP protocol /some/page.html is asked for.
